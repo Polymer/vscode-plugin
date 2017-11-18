0958dc3b76e4c727e2e5acd462c4c51165eb6389
Peter Burns <rictic@google.com>
2017-11-18 12:47:49 -0800

    Hacky workaround no longer needed.
    The server just needed to register for willSaveWaitUntil notifications.

--- src/extension.ts
+++ src/extension.ts
@@ -50,47 +50,6 @@ export function activate(context: ExtensionContext) {
   const disposable = languageClient.start();
   context.subscriptions.push(disposable);
 
-  // This is a workaround because it appears that vscode does not call the
-  // language server's onWillSaveTextDocument method, so we must do it
-  // ourselves.
-  // https://github.com/Microsoft/vscode-languageserver-node/issues/274
-  workspace.onWillSaveTextDocument((event) => {
-    if (!workspace.getConfiguration('polymer-ide').get('fixOnSave')) {
-      return;
-    }
-    event.waitUntil((async () => {
-      const rawTextEdits = (await languageClient.sendRequest(
-                               'polymer-ide/getAllFixesForFile',
-                               event.document.uri.toString())) as TextEditRaw[];
-      const edits = convertEditsFromProtocol(rawTextEdits);
-      return edits;
-    })());
-  });
-  function convertWorkspaceEditFromProtocol(rawEdit: WorkspaceEditRaw) {
-    const edit = new WorkspaceEdit();
-    const changes = rawEdit.changes!;
-    for (const uri of Object.keys(changes)) {
-      for (const change of changes[uri]) {
-        const range = convertRangeFromProtocol(change.range);
-        edit.replace(Uri.parse(uri), range, change.newText);
-      }
-    }
-    return edit;
-  }
-  function convertEditsFromProtocol(rawEdits: TextEditRaw[]): TextEdit[] {
-    const result = [];
-    for (const rawEdit of rawEdits) {
-      result.push(TextEdit.replace(
-          convertRangeFromProtocol(rawEdit.range), rawEdit.newText));
-    }
-    return result;
-  }
-  function convertRangeFromProtocol(range: RangeRaw): Range {
-    return new Range(
-        new Position(range.start.line, range.start.character),
-        new Position(range.end.line, range.end.character));
-  }
-
   // Push the disposable to the context's subscriptions so that the
   // client can be deactivated on extension deactivation
   context.subscriptions.push(disposable);
